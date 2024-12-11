ceae21c54553e8e1809db912e10b1a8e5200e9e5
ML SYSTEMS ADMIN <76473133+mlsysadmin@users.noreply.github.com>
2024-12-11 15:02:48 +0800

    v3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Oracle Cloud DevOps example' });
+  res.render('index', { title: 'REGAN SAMPLE' });
 });
 
 module.exports = router;
